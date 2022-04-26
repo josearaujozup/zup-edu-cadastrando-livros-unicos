0517bc75103bbb27b345c61c75649d919c3d48c2
Denes <jose.araujo@zup.com.br>
2022-04-26 15:15:12 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/livrosunicos/LivrosunicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.livrosunicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosunicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosunicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/livrosunicos/LivrosunicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.livrosunicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosunicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
